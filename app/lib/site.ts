f23ff07f10ad700e497f4456b7158fda3b873bd9
daanny214 <daanny214@gmail.com>
2026-06-13 00:16:26 +0100

    update LinkedIn URL and public contact email

--- app/lib/site.ts
+++ app/lib/site.ts
@@ -1,13 +1,13 @@
 export const site = {
   name: "Daniel Isiyemi",
   role: "Web & Mobile Application Developer",
-  email: "disiyemi@gmail.com",
+  email: "daanny214@gmail.com",
   phone: "+234 8106660790",
   phoneHref: "tel:+2348106660790",
   location: "Lagos, Nigeria",
   github: "https://github.com/daanny214",
-  // TODO: replace with your real LinkedIn and Twitter/X profile URLs
-  linkedin: "https://linkedin.com/in/yourusername",
+  linkedin: "https://linkedin.com/in/daniel-isiyemi-b81954253",
+  // TODO: replace with your real Twitter/X profile URL
   twitter: "https://twitter.com/yourusername",
 };
 
